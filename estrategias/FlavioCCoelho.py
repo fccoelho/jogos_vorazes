4f7f8e99ccd7945a0df55e6b4ac623e83b8e30c7
Flavio C. Coelho <fccoelho@gmail.com>
2014-01-22 09:41:46 -0200

    consertei bugs do simulador

--- estrategias/FlavioCCoelho.py
+++ estrategias/FlavioCCoelho.py
@@ -1,7 +1,7 @@
 __author__ = 'fccoelho'
 
 from jogadores import Jogador
-import random
+
 
 class MeuJogador(Jogador):
     def escolha_de_cacada(self, rodada, comida_atual, reputacao_atual, m, reputacoes_dos_jogadores):
